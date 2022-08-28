b333dcb7276d489ea37555cd0322f21e6422d11c
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-08-28 14:57:38 -0300

    departamento criado

---
+++ src/program/Main.java
@@ -0,0 +1,14 @@
+package program;
+
+import model.entities.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1,"livros");
+		System.out.println(obj);
+
+	}
+
+}
