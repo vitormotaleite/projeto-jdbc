538c26e90edbd9d69b24e9f3c18c678f76954a92
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-08-28 15:44:20 -0300

    reusando a instanciacao

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,13 +1,20 @@
 package program;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(1,"livros");
-		System.out.println(obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("=== TEST 1: seller find by id ===");
+		
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 
 	}
 
