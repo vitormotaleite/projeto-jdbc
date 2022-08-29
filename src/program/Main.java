aa5b93d657c5ffbe798699c03d0b47390caa5100
vitormotaleite <vitor.mota.diniz@ccc.ufcg.edu.br>
2022-08-29 09:36:43 -0300

    find all implementado

--- src/program/Main.java
+++ src/program/Main.java
@@ -25,6 +25,14 @@ public class Main {
 		for(Seller obj:list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("=== TEST 3: seller find all ===");
+		list = sellerDao.findAll();
+		
+		for(Seller obj:list) {
+			System.out.println(obj);
+		}
+
 
 	}
 
